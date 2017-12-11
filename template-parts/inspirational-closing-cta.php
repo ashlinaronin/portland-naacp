47dda5a2cc08d4504a0cfe1233168b8d93f98cfc
ashlinaronin <ashlinaronin@gmail.com>
2017-12-10 22:25:55 -0800

    Use ACF fields and subfields in closing CTA partial

--- template-parts/inspirational-closing-cta.php
+++ template-parts/inspirational-closing-cta.php
@@ -1,13 +1,29 @@
 <?php
-
+  $link_1 = get_sub_field('link_1');
+  $link_2 = get_sub_field('link_2');
 ?>
 
 <div class="module__closing-cta callout large text-center">
     <div>
-        <h3>Inspirational closing call-to-action</h3>
+        <h3><?php the_field('footer_message'); ?></h3>
     </div>
     <div>
-        <button class="button">Donate</button>
-        <button class="button">Join today</button>
+
+      <?php
+        if (have_rows('footer_cta-links')):
+          while(have_rows('footer_cta-links')): the_row();
+            $link_1 = get_sub_field('link_1');
+            $link_2 = get_sub_field('link_2');
+
+            if ($link_1): ?>
+              <a class="button" href="<?php echo $link_1['url']; ?>" target="<?php echo $link_1['target']; ?>"><?php echo $link_1['title']; ?></a>
+            <?php endif;
+            if ($link_2): ?>
+              <a class="button" href="<?php echo $link_2['url']; ?>" target="<?php echo $link_2['target']; ?>"><?php echo $link_2['title']; ?></a>
+            <?php endif;
+          endwhile;
+        endif;
+      ?>
+
     </div>
 </div>
