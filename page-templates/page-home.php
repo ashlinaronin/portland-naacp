788a6f3d743485053daf1289a79e492e7d293725
ashlinaronin <ashlinaronin@gmail.com>
2017-12-18 21:57:33 -0800

    Add styles to events section to mostly match comp.
    We can fine-tune later once the design is finalized, but this gets us most of the way there.

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -14,8 +14,8 @@ get_header(); ?>
       </div>
     </div>
 
-    <div class="module-events callout large text-center">
-      <div class="module-events__header callout">
+    <div class="module-events callout large">
+      <div class="module-events__header callout text-center">
         <h2>Next general session</h2>
         <h1>Sunday, August 27th</h1>
         <h5>12-2 PM | Concordia University Hall</h5>
@@ -24,62 +24,42 @@ get_header(); ?>
           <a class="button hollow">More info</a>
         </div>
       </div>
-      <h2>Upcoming events</h2>
-      <div class="module-events__events grid-x grid-margin-2">
-        <div class="module-events__event large-auto cell card">
-          <div>
-            <h3>Title Goes Here</h3>
+      <h2 class="text-center">Upcoming events</h2>
+      <div class="module-events__events grid-x grid-margin-2 align-justify align-middle">
+        <div class="module-events__event large-auto cell">
+          <div class="module-events__event-content">
+            <a>10/08</a>
+            <h5>Political Action Committee Meeting</h5>
           </div>
-          <div class="card-section">
-            <p>Day</p>
-            <p>Date</p>
-            <p>Time</p>
-          </div>
-          <div class="card-section">
-            <hr />
-            <a href="#">Learn More</a>
+          <div class="module-events__event-cta">
+            <a class="button expanded">View details ></a>
           </div>
         </div>
-        <div class="module-events__event large-auto cell card">
-          <div>
-            <h3>Title Goes Here</h3>
+        <div class="module-events__event large-auto cell">
+          <div class="module-events__event-content">
+            <a>10/08</a>
+            <h5>Political Action Committee Meeting</h5>
           </div>
-          <div class="card-section">
-            <p>Day</p>
-            <p>Date</p>
-            <p>Time</p>
-          </div>
-          <div class="card-section">
-            <hr />
-            <a href="#">Learn More</a>
+          <div class="module-events__event-cta">
+            <a class="button expanded">View details ></a>
           </div>
         </div>
-        <div class="module-events__event large-auto cell card">
-          <div>
-            <h3>Title Goes Here</h3>
+        <div class="module-events__event large-auto cell">
+          <div class="module-events__event-content">
+            <a>10/08</a>
+            <h5>Political Action Committee Meeting</h5>
           </div>
-          <div class="card-section">
-            <p>Day</p>
-            <p>Date</p>
-            <p>Time</p>
-          </div>
-          <div class="card-section">
-            <hr />
-            <a href="#">Learn More</a>
+          <div class="module-events__event-cta">
+            <a class="button expanded">View details ></a>
           </div>
         </div>
-        <div class="module-events__event large-auto cell card">
-          <div>
-            <h3>Title Goes Here</h3>
+        <div class="module-events__event large-auto cell">
+          <div class="module-events__event-content">
+            <a>10/08</a>
+            <h5>Political Action Committee Meeting</h5>
           </div>
-          <div class="card-section">
-            <p>Day</p>
-            <p>Date</p>
-            <p>Time</p>
-          </div>
-          <div class="card-section">
-            <hr />
-            <a href="#">Learn More</a>
+          <div class="module-events__event-cta">
+            <a class="button expanded">View details ></a>
           </div>
         </div>
       </div>
