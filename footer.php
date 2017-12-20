5f58b80b47b894c96d659a60cb07687cd040a897
ashlinaronin <ashlinaronin@gmail.com>
2017-12-19 21:50:36 -0800

    First draft of upper footer

--- footer.php
+++ footer.php
@@ -18,61 +18,61 @@
 
 
         <!-- Upper Footer -->
-        <div class="grid-x grid-padding-x">
-          <div class="auto cell">
-            <h5>Get Involved</h5>
-          </div>
-          <div class="auto cell">
-            <h5>Issues</h5>
-          </div>
-          <div class="auto cell">
-            <h5>News</h5>
-            <ul class="menu align-top">
+        <ul class="footer__upper-container menu align-spaced">
+          <li>
+            <a href="#"><h4>Get Involved</h4></a>
+          </li>
+          <li>
+            <a href="#"><h4>Issues</h4></a>
+          </li>
+          <li>
+            <a href="#"><h4>News</h4></a>
+            <ul class="menu vertical footer__sub-category-list">
               <li><a href="#">Category 1</a></li>
               <li><a href="#">Category 2</a></li>
               <li><a href="#">Category 3</a></li>
             </ul>
-          </div>
-          <div class="auto cell">
-            <h5>Calendar</h5>
-          </div>
-          <div class="auto cell">
-            <h5>About</h5>
-            <ul class="menu">
+          </li>
+          <li>
+            <a href="#"><h4>Calendar</h4></a>
+          </li>
+          <li>
+            <a href="#"><h4>About</h4></a>
+            <ul class="menu vertical footer__sub-category-list">
               <li><a href="#">Committees</a></li>
               <li><a href="#">History</a></li>
               <li><a href="#">Contact</a></li>
             </ul>
-          </div>
-          <div class="small-5 cell">
-            <div>
+          </li>
+          <li>
+            <div class="sdf">
               <label for="updates-email">Get updates</label>
               <div class="input-group">
-                <input id="updates-email" class="input-group-field" type="email">
+                <input id="updates-email" class="input-group-field" type="email" placeholder="Enter your email address">
                 <div class="input-group-button">
                   <input type="submit" class="button" value="Sign Up">
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          </li>
+        </ul>
 
 
         <!-- Lower Footer -->
-        <div>
-          <ul class="menu menu-horizontal">
-            <li><a href="#">Donate</a></li>
-            <li><a href="#">Join</a></li>
-            <li><a href="#">T</a></li>
-            <li><a href="#">F</a></li>
-          </ul>
-          <ul class="menu menu-horizontal">
-            <li><a href="#">Portland NAACP Chapter Information</a></li>
-            <li><a href="#">Address</a></li>
-            <li><a href="#">Main Contact Email</a></li>
-            <li><a href="#">Etc.</a></li>
-          </ul>
-        </div>
+<!--        <div>-->
+<!--          <ul class="menu menu-horizontal">-->
+<!--            <li><a href="#">Donate</a></li>-->
+<!--            <li><a href="#">Join</a></li>-->
+<!--            <li><a href="#">T</a></li>-->
+<!--            <li><a href="#">F</a></li>-->
+<!--          </ul>-->
+<!--          <ul class="menu menu-horizontal">-->
+<!--            <li><a href="#">Portland NAACP Chapter Information</a></li>-->
+<!--            <li><a href="#">Address</a></li>-->
+<!--            <li><a href="#">Main Contact Email</a></li>-->
+<!--            <li><a href="#">Etc.</a></li>-->
+<!--          </ul>-->
+<!--        </div>-->
 
 				<?php do_action( 'foundationpress_after_footer' ); ?>
 			</footer>
