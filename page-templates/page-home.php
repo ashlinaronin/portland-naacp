e8b20a7f055b2fd43ef38af2f10f0756ce798bb1
Daniel Portis-Cathers <deepseamusic@gmail.com>
2018-01-31 18:49:31 -0800

    front page buttons

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -7,16 +7,16 @@ get_header(); ?>
 <?php get_template_part( 'template-parts/featured-image' ); ?>
 
     <div class="c-hero callout large text-center">
-      <h1>Inspiring intro message</h1>
+      <h1>Ensuring the <span>political</span>, <span>educational</span>, <span>social</span> and <span>economic equality of rights</span> of all persons and to <span>eliminate racial hatred and discrimination</span></h1>
       <div class="expanded stacked-for-small button-group margin-horizontal-2">
-        <a class="button hollow">Learn more</a>
-        <a class="button">Join us</a>
+        <a class="button">Learn more</a>
+        <a class="button hollow">Join us</a>
       </div>
     </div>
 
     <div class="module-events callout large">
       <div class="module-events__header callout text-center">
-        <h2>Next general session</h2>
+        <h3>Next general session</h3>
         <h1>Sunday, August 27th</h1>
         <h5>12-2 PM | Concordia University Hall</h5>
         <div class="small stacked-for-small button-group align-center">
