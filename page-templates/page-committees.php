e8339e02390356b83ef184ccc32c29d0adcf8574
Daniel Portis-Cathers <deepseamusic@gmail.com>
2018-04-06 16:59:39 -0700

    New page-committees.php, _images.scss files

--- page-templates/page-committees.php
+++ page-templates/page-committees.php
@@ -1,32 +1,41 @@
 <?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
-
- get_header(); ?>
+/*
+Template Name: Committees
+*/
+get_header(); ?>
 
  <?php get_template_part( 'template-parts/featured-image' ); ?>
 
- <div class="main-wrap" role="main">
+ 
+<div class="main-wrap" role="main">
 
  <?php do_action( 'foundationpress_before_content' ); ?>
  <?php while ( have_posts() ) : the_post(); ?>
 	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-		<header>This is page.php
+		<header>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header>
 		<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 		<div class="entry-content">
 			<?php the_content(); ?>
 			<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+			
+
+<?php // display ACF Committee fields - dpc180405 ?>
+			
+	<?php while( have_rows('Committees') ): the_row(); ?> 
+			<h3><?php the_sub_field('Committee_name'); ?></h3> 
+			<p class="committee-description"><?php the_sub_field('Committee_description'); ?></p> 
+			<img src="<?php the_sub_field('image'); ?>" class="float-right committee" alt=""/>        
+			<p class="clearfloat">&nbsp;</p>
+	<?php endwhile; ?> 
+			
+			
 		</div>
+		
+		
+		
+		
 		<footer>
 			<?php
 				wp_link_pages(
