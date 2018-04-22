c4f2d4aae4b3c13b7741096242d2eb6dd1b621e2
ashlinaronin <ashlinaronin@gmail.com>
2018-04-21 18:42:28 -0700

    Add orange text color modifier and use for single post meta

--- library/entry-meta.php
+++ library/entry-meta.php
@@ -9,7 +9,7 @@
 if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function foundationpress_entry_meta() {
 		/* translators: %1$s: current date, %2$s: current time */
-		echo '<h5><time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( '%1$s', 'foundationpress' ), get_the_date() ) . '</time></h5>';
+		echo '<h5 class="text-orange"><time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( '%1$s', 'foundationpress' ), get_the_date() ) . '</time></h5>';
 		echo '<p class="byline author">' . __( 'Posted by', 'foundationpress' ) . ' <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" rel="author" class="fn">' . get_the_author() . '</a></p>';
 	}
 endif;
