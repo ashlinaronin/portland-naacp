2526989306e1cc884be90d244c16378cf73d651e
adamtrabold <adamtrabold@gmail.com>
2018-04-25 22:48:32 -0700

    adjusted 'get involved' page, as well as issues page. reformatted code for issues page...will be going through the others to adjust next.

--- page-templates/page-issues.php
+++ page-templates/page-issues.php
@@ -4,62 +4,58 @@ Template Name: Issues
 */
 get_header(); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
 
-<div class="text-center content-container">
-  <div class="callout large callout-issues lead-in">
-    <div class="callout secondary">
-<?php // display ACF Committee fields (Lead-in) - dpc180407 ?>
-			
-<h2><?php the_field('lead-in_heading'); ?></h2>
-<p><?php the_field('lead-in_copy'); ?></p>
-
-		    </div>
-  </div>
-
-<?php //display ACF Issues Title/Description fields ?>
-	<div class="callout large callout-issues focus">
-  <?php if ( have_rows('issues')): ?>
-    <?php while( have_rows('issues')): the_row(); ?>
-    <?php $issue = get_sub_field('issue'); ?>
-      <?php $issue_description = get_sub_field('issue_description');?>
-      <div class="callout secondary">
-        <h3><?php echo $issue; ?></h3>
-	      <?php echo $issue_description; ?>
+<?php // lead-in?>
+<div class="lead-in text-center">
+  <div class="grid-container">
+    <div class="grid-x">
+      <div class="cell small-12">
+        <h1><?php the_field('lead-in_heading'); ?></h1>
+        <h5 class="medium-8 medium-offset-2"><?php the_field('lead-in_copy'); ?></h5>
       </div>
-    <?php endwhile; ?>
-  <?php endif; ?>
+    </div>
   </div>
+</div>
 
-  <div class="callout large secondary callout-issues">
-    <h3><?php get_field('headline-description'); ?></h3>
-
-		<div class="card-divider"></div>
+<div class="grid-container content">
+  <?php // issues ?>
+  <div class="grid-x main-content">
+  	<div class="cell small-12 medium-8 medium-offset-2">
+      <?php if ( have_rows('issues')): ?>
+      <?php while( have_rows('issues')): the_row(); ?>
+        <?php $issue = get_sub_field('issue'); ?>
+        <?php $issue_description = get_sub_field('issue_description');?>
+        <h4><?php echo $issue; ?></h4>
+	      <?php echo $issue_description; ?>
+      <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+  
+  <?php // three-column section ?>
+  <div class="grid-x three-column-links">
     <?php if ( have_rows('links')): ?>
-      <div class="grid-x">
       <?php while( have_rows('links')): the_row();
         $link1 = get_sub_field('link_1');
         $link2 = get_sub_field('link_2');
     		$link3 = get_sub_field('link_3');
       ?>
-
-        <div class="stacked-for-small small-4 cell card">
-          <h4><?php echo($link1['headline']) ?></h4>
-          <p><?php echo($link1['copy']) ?></p>
-          <a href="<?php echo($link1['link']['url']); ?>" target="<?php echo($link1['link']['target']); ?>"><?php echo($link1['link']['title']); ?></a>
-        </div>
-        <div class="stacked-for-small small-4 cell card">
-          <h4><?php echo($link2['headline']) ?></h4>
-          <p><?php echo($link2['copy']) ?></p>
-          <a href="<?php echo($link2['link']['url']); ?>" target="<?php echo($link2['link']['target']); ?>"><?php echo($link2['link']['title']); ?></a>
-        </div>
-        <div class="stacked-for-small small-4 cell card">
-          <h4><?php echo($link3['headline']) ?></h4>
-          <p><?php echo($link3['copy']) ?></p>
-          <a href="<?php echo($link3['link']['url']); ?>" target="<?php echo($link3['link']['target']); ?>"><?php echo($link3['link']['title']); ?></a>
-        </div>
-      <?php endwhile; ?>
+      <div class="cell small-12 medium-4">
+        <h5><?php echo($link1['headline']) ?></h5>
+        <p><?php echo($link1['copy']) ?></p>
+        <a href="<?php echo($link1['link']['url']); ?>" target="<?php echo($link1['link']['target']); ?>"><?php echo($link1['link']['title']); ?></a>
       </div>
+      <div class="cell small-12 medium-4">
+        <h5><?php echo($link2['headline']) ?></h5>
+        <p><?php echo($link2['copy']) ?></p>
+        <a href="<?php echo($link2['link']['url']); ?>" target="<?php echo($link2['link']['target']); ?>"><?php echo($link2['link']['title']); ?></a>
+      </div>
+      <div class="cell small-12 medium-4">
+        <h5><?php echo($link3['headline']) ?></h5>
+        <p><?php echo($link3['copy']) ?></p>
+        <a href="<?php echo($link3['link']['url']); ?>" target="<?php echo($link3['link']['target']); ?>"><?php echo($link3['link']['title']); ?></a>
+      </div>
+      <?php endwhile; ?>
     <?php endif; ?>
   </div>
 </div>
