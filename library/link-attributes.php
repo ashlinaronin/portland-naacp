ecb459c83e4ce1808ac031a6aa191c10a56f50e3
ashlinaronin <ashlinaronin@gmail.com>
2018-04-28 17:23:24 -0700

    Swap decorated link attrs to match comp

--- library/link-attributes.php
+++ library/link-attributes.php
@@ -4,9 +4,10 @@ add_filter('next_posts_link_attributes', 'decorated_link_classes_next');
 add_filter('previous_posts_link_attributes', 'decorated_link_classes_previous');
 
 function decorated_link_classes_previous() {
-return 'class="decorated-link decorated-link--single-left"';
+	return 'class="decorated-link decorated-link--single-right"';
+
 }
 
 function decorated_link_classes_next() {
-return 'class="decorated-link decorated-link--single-right"';
+	return 'class="decorated-link decorated-link--single-left"';
 }
