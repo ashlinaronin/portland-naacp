06f6cdccac13d345003899d478f635b353dd07a8
adamtrabold <adamtrabold@gmail.com>
2018-04-29 22:29:17 -0700

    contact page styling

---
+++ page-templates/page-contact.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+Template Name: Contact
+*/
+
+ get_header(); ?>
+
+ <?php // get_template_part( 'template-parts/featured-image' ); ?>
+
+<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+
+<?php // lead-in?>
+<div class="lead-in text-center">
+  <div class="grid-container">
+    <div class="grid-x">
+      <div class="cell small-12 medium-10 medium-offset-1">
+        <h1><?php the_field('lead-in_heading'); ?></h1>
+        <?php 
+        if(get_field('lead-in_copy')) {
+          echo '<h3>' . get_field('lead-in_copy') . '</h3>';
+        }
+
+        if (have_rows('lead-in_CTA')):
+          while(have_rows('lead-in_CTA')): the_row();
+            $link_1 = get_sub_field('lead-in_link-1');
+            $link_2 = get_sub_field('lead-in_link-2');
+            $link_3 = get_sub_field('lead-in_link-3');
+      ?>
+           <div class="expanded stacked-for-small button-group button-group-get-involved">
+
+              <?php if ($link_1): ?>
+              <a class="button button-get-involved" href="<?php echo $link_1['url']; ?>"
+                 target="<?php echo $link_1['target']; ?>"><?php echo $link_1['title']; ?></a>
+              <?php endif; ?>
+              <?php if ($link_2): ?>
+                  <a class="button hollow button-get-involved" href="<?php echo $link_2['url']; ?>"
+                     target="<?php echo $link_2['target']; ?>"><?php echo $link_2['title']; ?></a>
+              <?php endif; ?>
+              <?php if ($link_3): ?>
+                  <a class="button button-get-involved last" href="<?php echo $link_3['url']; ?>"
+                     target="<?php echo $link_3['target']; ?>"><?php echo $link_3['title']; ?></a>
+              <?php endif; ?>
+            </div>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="grid-container" role="main">
+ 	<div class="grid-x">
+		<div class="cell small-12 medium-6 medium-offset-3">
+
+			<?php // Contact form
+			if (get_field('form_shortcode')) {
+				$shortcode = get_field('form_shortcode');
+
+				echo do_shortcode($shortcode);
+			} ?>
+			
+			<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+
+		</div>
+	</div>
+</div>
+<?php endwhile;?>
+<?php do_action( 'foundationpress_after_content' ); ?>
+
+ <?php get_footer();
--- page.php
+++ page.php
@@ -12,47 +12,51 @@
 
  get_header(); ?>
 
- <?php get_template_part( 'template-parts/featured-image' ); ?>
+ <?php // get_template_part( 'template-parts/featured-image' ); ?>
 
- <div class="main-wrap" role="main">
+ <div class="grid-container" role="main">
+ 	<div class="grid-x">
 
- <?php do_action( 'foundationpress_before_content' ); ?>
- <?php while ( have_posts() ) : the_post(); ?>
-	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-		<header>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header>
-		<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-		<div class="entry-content">
-			<?php the_content(); ?>
-			
-				<?php // display ACF Committee fields (Lead-in) - dpc180407 ?>
+		 <?php do_action( 'foundationpress_before_content' ); ?>
+		 <?php while ( have_posts() ) : the_post(); ?>
+			<div class="cell small-12 medium-8">
+				<header>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
+				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+					
+						<?php // display ACF Committee fields (Lead-in) - dpc180407 ?>
 
-				<h2><?php the_field('lead-in_heading'); ?></h2>
-				<p><?php the_field('lead-in_copy'); ?></p>
-			
-			<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+						<h2><?php the_field('lead-in_heading'); ?></h2>
+						<p><?php the_field('lead-in_copy'); ?></p>
+					
+					<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+				</div>
+				<footer>
+					<?php
+						wp_link_pages(
+							array(
+								'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
+								'after'  => '</p></nav>',
+							)
+						);
+					?>
+					<p><?php the_tags(); ?></p>
+				</footer>
+				<?php do_action( 'foundationpress_page_before_comments' ); ?>
+				<?php comments_template(); ?>
+				<?php do_action( 'foundationpress_page_after_comments' ); ?>
+			</div>
+		<?php endwhile;?>
+
+		<?php do_action( 'foundationpress_after_content' ); ?>
+		<div class="cell small-12 medium-4">
+			<?php get_sidebar(); ?>
 		</div>
-		<footer>
-			<?php
-				wp_link_pages(
-					array(
-						'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-						'after'  => '</p></nav>',
-					)
-				);
-			?>
-			<p><?php the_tags(); ?></p>
-		</footer>
-		<?php do_action( 'foundationpress_page_before_comments' ); ?>
-		<?php comments_template(); ?>
-		<?php do_action( 'foundationpress_page_after_comments' ); ?>
-	</article>
- <?php endwhile;?>
 
- <?php do_action( 'foundationpress_after_content' ); ?>
- <?php get_sidebar(); ?>
-
- </div>
+	</div>
+</div>
 
  <?php get_footer();
