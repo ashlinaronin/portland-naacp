340268a60aeadb6a439272596c1f20671e3f16b5
adamtrabold <adamtrabold@gmail.com>
2018-05-03 21:04:34 -0700

    committee email additions

--- page-templates/page-committees.php
+++ page-templates/page-committees.php
@@ -34,7 +34,10 @@ get_header(); ?>
 					// check if the repeater field has rows of data
 					if ( have_rows( 'committee_information' ) ):
 					  // loop through the rows of data
-					  while ( have_rows( 'committee_information' ) ) : the_row(); ?>
+					  while ( have_rows( 'committee_information' ) ) : the_row(); 
+  						  	$committee_email = get_sub_field( 'committee_email' );
+				  	?>
+
 					  	<div class="committee">
 
 						    <!--display sub field values-->
@@ -43,6 +46,9 @@ get_header(); ?>
 							<!--Committee Description-->
 						 	<div class="committee-description cell small-12">
 								<?php the_sub_field( 'description' ); ?>
+								<?php if ( ! empty( $committee_email ) ): ?>
+									<p><b>Get in touch:</b> <?php echo $committee_email; ?></p>
+								<?php endif; ?>
 							</div>
 
 							<!-- Committee members images -->
@@ -50,11 +56,16 @@ get_header(); ?>
 		                      	<h6>Committee Leadership</h6>
 								<?php
 									// check if the repeater field has rows of data
-									if ( have_rows( 'images' ) ):
+									if ( have_rows( 'members' ) ):
 										// loop through the rows of data
-										while ( have_rows( 'images' ) ) : the_row(); ?>
+										while ( have_rows( 'members' ) ) : the_row(); ?>
 											<?php
 											$image = get_sub_field( 'image' );
+											$member_name = get_sub_field( 'member_name' );
+											$member_title = get_sub_field( 'member_title' );
+											$member_email = get_sub_field( 'member_email' );
+
+											// Grab image info
 											if ( ! empty( $image ) ):
 
 												// vars
@@ -67,18 +78,17 @@ get_header(); ?>
 												$thumb  = $image['sizes'][ $size ];
 												$width  = $image['sizes'][ $size . '-width' ];
 												$height = $image['sizes'][ $size . '-height' ];
-
-												//thumbnail image plus caption
-												//if ( $caption ): ?>
+											?>
+							                    
 							                    <div class="committee-leader">
-													<?php //endif; ?>
 													<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>"
 													title="<?php echo $title; ?>" width="<?php echo $width; ?>"
 													height="<?php echo $height; ?>"/>
-													<?php if ( $caption ): ?>
-														<p class="wp-caption-text"><?php echo $caption; ?></p>
+													<p class="member-info name"><?php echo $member_name; ?></p>
+													<p class="member-info title"><?php echo $member_title; ?></p>
+													<?php if ( ! empty( $member_email ) ): ?>
+														<p class="member-info email"><?php echo $member_email; ?></p>
 													<?php endif; ?>
-													<p class="clear"></p>
 												</div>
 											<?php endif; ?>
 										<?php endwhile;
